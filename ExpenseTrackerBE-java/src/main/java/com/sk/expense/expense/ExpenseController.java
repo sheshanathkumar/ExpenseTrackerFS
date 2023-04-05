b4dbe4dccc3554e107a6cb75a106c7be855f6d0e
Sheshanath <35036388+sheshanathkumar@users.noreply.github.com>
2023-04-05 19:51:09 +0530

    added deleted button

--- ExpenseTrackerBE-java/src/main/java/com/sk/expense/expense/ExpenseController.java
+++ ExpenseTrackerBE-java/src/main/java/com/sk/expense/expense/ExpenseController.java
@@ -16,6 +16,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -26,10 +27,11 @@ public class ExpenseController {
 
     public static final String[] monthArr = {"", "Jan", "Feb", "March", "Apr", "may", "Jun", "July", "Aug", "Sep", "Oct", "Nov", "Dec"};
 
-    private String getFileName () {
+    private String getFileName() {
         LocalDate date = LocalDate.now();
-        String fileName = monthArr[date.getDayOfMonth()] ;
-        return fileName + ".json" ;
+        String fileName = monthArr[date.getMonthValue()];
+        log.info("current file name {}", fileName);
+        return fileName + ".json";
     }
 
 
@@ -38,17 +40,21 @@ public class ExpenseController {
     public ResponseEntity<List<Transaction>> getHistory() throws IOException {
         log.info("start getHistory");
 
-        File file = new File(getFileName()) ;
+        File file = new File(getFileName());
         if (file.exists()) {
-            Transaction[] traArr = mapper.readValue(file, Transaction[] .class);
-            List<Transaction> transactions = Arrays.asList(traArr) ;
-            if (transactions.isEmpty()) {
+            try {
+                Transaction[] traArr = mapper.readValue(file, Transaction[].class);
+                List<Transaction> transactions = Arrays.asList(traArr);
+                if (transactions.isEmpty()) {
+                    return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+                } else {
+                    Collections.sort(transactions, Collections.reverseOrder());
+                    return new ResponseEntity<>(transactions, HttpStatus.OK);
+                }
+            } catch (Exception e) {
                 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-            } else {
-                Collections.sort(transactions , Collections.reverseOrder() );
-                return new ResponseEntity<>(transactions, HttpStatus.OK);
             }
-        } else{
+        } else {
             file.createNewFile();
             log.error("{} File not found", file.getName());
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -58,23 +64,21 @@ public class ExpenseController {
 
     @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping(path = "/addtransaction")
-    public ResponseEntity<List<Transaction>> addTransaction( @RequestBody Transaction transaction) throws IOException {
+    public ResponseEntity<ApiResponse> addTransaction(@RequestBody Transaction transaction) throws IOException {
         log.info("start addtransaction {}", transaction);
         List<Transaction> transactionList = new ArrayList<>();
 
-        if (! StringUtils.hasLength(transaction.getSource()) || ! StringUtils.hasLength(transaction.getCategory())){
+        if (!StringUtils.hasLength(transaction.getSource()) || !StringUtils.hasLength(transaction.getCategory())) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
 
         try {
 
-            File file = new File(getFileName()) ;
-            if (! file.exists()) {
-                file.createNewFile();
-            }
+            File file = new File(getFileName());
+
             try {
                 transactionList = mapper.readValue(file, List.class);
-            }catch (Exception e) {
+            } catch (Exception e) {
                 transactionList = new ArrayList<>();
             }
             transaction.setId(transactionList.size() + 1);
@@ -89,12 +93,35 @@ public class ExpenseController {
                 fw.write(transStr);
             }
 
-            return new ResponseEntity<>(transactionList, HttpStatus.OK);
+            return new ResponseEntity<>(new ApiResponse("SUCCESS", HttpStatus.OK), HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.BAD_GATEWAY);
         }
 
 
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @DeleteMapping(path = "/deleteTransaction")
+    public ResponseEntity<ApiResponse> deleteTransaction(@RequestParam("id") Integer id) throws IOException {
+        log.info("start delete transaction {}", id);
+        List<Transaction> transactionList = new ArrayList<>();
+
+        File file = new File(getFileName());
+        try {
+            Transaction [] trArr = mapper.readValue(file, Transaction [].class);
+            transactionList =  Arrays.asList(trArr).stream().filter(x -> id != x.getId()).collect(Collectors.toList());
+            String transStr = mapper.writeValueAsString(transactionList);
+            if (file.exists()) {
+                file.delete();
+            }
+            try (FileWriter fw = new FileWriter(file)) {
+                fw.write(transStr);
+            }
+            return new ResponseEntity<>(new ApiResponse("SUCCESS", HttpStatus.OK), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ApiResponse("FAILED", HttpStatus.BAD_REQUEST), HttpStatus.BAD_REQUEST);
+        }
     }
 
 
